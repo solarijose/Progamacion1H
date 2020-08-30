ad2c1b3665cd1c1b25f26ba3794cba48664d6c97
solarijose <solarijose@gmail.com>
2020-08-30 18:30:43 -0300

    Primer commit

---
+++ Ejercicio 1/src/Ejercicio 1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Ejercicio.c
+ Author      : Solari Jose Luis
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Ejercicio 1"); /* prints Ejercicio 1 */
+	return EXIT_SUCCESS;
+}
